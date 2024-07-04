b2692eb0c0951dbfc77694dac1f4c643782fc079
Rinchen <yonzanrinchen@gmail.com>
2024-07-04 18:11:57 +0545

    Merge remote changes

--- app/src/main/java/com/example/bikenepal/controller/retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/bikenepal/controller/retrofit/RetrofitClient.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.bikenepal.MainActivity;
 import com.example.bikenepal.model.ContactModel;
 import com.example.bikenepal.model.LoginModel;
 import com.example.bikenepal.model.UserModel;
+
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.UnknownHostException;
@@ -20,33 +23,38 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient extends AppCompatActivity {
 
-    // RetrofitClient connection established
-    public retrofit2.Retrofit retrofit = new retrofit2.Retrofit.Builder()
-            .baseUrl("http://192.168.1.67/API/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
+    // Singleton instance for Retrofit
+    private static RetrofitClient instance = null;
+    private apiservice api;
 
-    // apiService creation
-    apiservice api = retrofit.create(apiservice.class);
+    public RetrofitClient() {
+        retrofit2.Retrofit retrofit = new retrofit2.Retrofit.Builder()
+                .baseUrl("http://192.168.1.67/API/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        api = retrofit.create(apiservice.class);
+    }
 
+    public static synchronized RetrofitClient getInstance() {
+        if (instance == null) {
+            instance = new RetrofitClient();
+        }
+        return instance;
+    }
 
     // Method on user register request and response
     public void userRegister(String username, String email, String phonenumber, String password) {
-
         UserModel userModel = new UserModel(username, email, phonenumber, password);
         Log.d("register", "Initiating user registration");
 
         Call<UserModel> call = api.register(userModel);
         call.enqueue(new Callback<UserModel>() {
-
-            // Method on response of user register
             @Override
             public void onResponse(Call<UserModel> call, Response<UserModel> response) {
                 if (response.isSuccessful()) {
                     Log.d("register", "User registered successfully");
                 } else {
                     Log.d("register", "Registration failed with response code: " + response.code());
-                    // Optionally, log the response error body
                     try {
                         if (response.errorBody() != null) {
                             Log.d("register", "Error: " + response.errorBody().string());
@@ -57,19 +65,13 @@ public class RetrofitClient extends AppCompatActivity {
                 }
             }
 
-
-            // Method on failure of user register
             @Override
             public void onFailure(Call<UserModel> call, Throwable t) {
                 Log.d("register", "Registration failed: " + t.getMessage());
-                // Log the request URL for debugging
                 Log.d("register", "Request URL: " + call.request().url());
             }
         });
     }
-
-
-
 
     // Method on user login request and response
     public void loginUser(String username, String password, Activity activity) {
@@ -78,86 +80,56 @@ public class RetrofitClient extends AppCompatActivity {
 
         Call<LoginModel> loginModelCall = api.login(loginModel);
         loginModelCall.enqueue(new Callback<LoginModel>() {
-
-            // Method on response of user login
             @Override
             public void onResponse(Call<LoginModel> call, Response<LoginModel> response) {
                 if (response.isSuccessful()) {
                     Log.d("login", "Successfully logged in");
-
                     Intent intent = new Intent(activity, MainActivity.class);
                     activity.startActivity(intent);
                 } else {
-
                     Log.d("login", "Login request failed with response code: " + response.code());
-
-                    // Check the response code and show appropriate message
                     String message;
                     if (response.code() == 401) {
-                        // 401 Unauthorized indicates incorrect username or password
                         message = "Incorrect username or password";
                     } else {
-                        // Other HTTP errors
                         message = "Login failed: " + response.message();
                     }
-
                     Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
-
                 }
             }
 
-
-            // Method on failure of user login
             @Override
             public void onFailure(Call<LoginModel> call, Throwable t) {
-                // Log the request URL for debugging
                 Log.d("login", "Request URL: " + call.request().url());
                 Log.d("login", "Login request failed: " + t.getMessage());
-
-
                 String message;
                 if (t instanceof UnknownHostException) {
-                    // Server not found
                     message = "Server not found";
                 } else if (t instanceof ConnectException) {
-                    // Could not connect to the server
                     message = "Unable to connect to the server";
                 } else if (t instanceof IOException) {
-                    // General network error
                     message = "Network error";
                 } else {
-                    // Other errors (e.g., JSON parsing, unexpected exceptions, etc.)
                     message = "Unexpected error occurred";
                 }
-
-                // Display a Toast message for failure
                 Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-
-
-
-
     // Method on contact message request and response
     public void contactSend(String company, String country, String name, String description) {
-
         ContactModel contactModel = new ContactModel(company, country, name, description);
-        Log.d("contact register", "Initiating contact user data..");
+        Log.d("contact register", "Initiating contact user data...");
 
         Call<ContactModel> Contactcall = api.contact(contactModel);
         Contactcall.enqueue(new Callback<ContactModel>() {
-
-
-            // Method on response of user contact message
             @Override
             public void onResponse(Call<ContactModel> call, Response<ContactModel> response) {
                 if (response.isSuccessful()) {
                     Log.d("contact register", "Contact data registered successfully");
                 } else {
                     Log.d("contact register", "Contact registration failed with response code: " + response.code());
-                    // Optionally, log the response error body
                     try {
                         if (response.errorBody() != null) {
                             Log.d("contact register", "Error: " + response.errorBody().string());
@@ -168,12 +140,9 @@ public class RetrofitClient extends AppCompatActivity {
                 }
             }
 
-
-            // Method on failure of user contact message
             @Override
             public void onFailure(Call<ContactModel> call, Throwable t) {
                 Log.d("contact register", "Contact registration failed: " + t.getMessage());
-                // Log the request URL for debugging
                 Log.d("contact register", "Request URL: " + call.request().url());
             }
         });
